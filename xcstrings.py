bc925d8a94b9121ecac2cccd41ad9f53fdc3f50a
borrring <zh.j.0219@gmail.com>
2023-10-30 16:59:07 +0800

    update
    Use English as the source language when translating into other languages to improve accuracy.
    Fix the Chinese display of json files.
    Remove xcstringsForXcode.py

--- xcstrings.py
+++ xcstrings.py
@@ -7,9 +7,9 @@ import time
 from googletrans import Translator
 
 # Global variables
-isInfoPlist = False
-languageIdentifiers = ['en', 'zh-Hans', 'zh-Hant']
-languageIdentifiersForGoogle = {
+is_info_plist = False
+LANGUAGE_IDENTIFIERS = ['en', 'zh-Hans', 'zh-Hant', 'es', 'pt-PT', 'ja', 'ko']
+LANGUAGE_IDENTIFIERS_FOR_GOOGLE = {
     'zh-Hans': 'zh-CN', 
     'zh-Hant': 'zh-TW',
     'zh-HK': 'zh-TW',
@@ -20,10 +20,10 @@ languageIdentifiersForGoogle = {
 def translate_string(string, target_language):
     translator = Translator()
     
-    if target_language not in languageIdentifiersForGoogle:
+    if target_language not in LANGUAGE_IDENTIFIERS_FOR_GOOGLE:
         dest = target_language
     else:
-        dest = languageIdentifiersForGoogle[target_language]
+        dest = LANGUAGE_IDENTIFIERS_FOR_GOOGLE[target_language]
 
     try:
         source_language = translator.detect(string).lang
@@ -33,17 +33,16 @@ def translate_string(string, target_language):
         translation = translator.translate(string, dest=dest)
     except Exception as e:
         print(e)
-        print("翻译超时，等待3秒后重新执行翻译...")
-        time.sleep(2)
+        print("Translation timeout, retrying after 1 seconds...")
+        time.sleep(1)
         return translate_string(string, target_language)
         
-
     print(f"{target_language}: {translation.text}")
     return translation.text
 
 def main():
     # Get all the keys of strings
-    with open(json_path, "r") as f:
+    with open(json_path, "r", encoding="utf-8") as f:
         json_data = json.load(f)
     strings_keys = list(json_data["strings"].keys())
 
@@ -60,62 +59,69 @@ def main():
         print(f"[{now_str}]\n", f"🔥{key_index + 1}/{len(strings_keys)}: {key}")
 
         strings = json_data["strings"][key]
+
         # The strings field is empty.
         if not strings:
-            strings = {
-                "extractionState": "manual",
-                "localizations": {},
-            }
+            strings = {"extractionState": "manual", "localizations": {}}
+        
         # The localizations field is empty
         if "localizations" not in strings:
             strings["localizations"] = {}
     
         localizations = strings["localizations"]
 
-        for language in languageIdentifiers:
+        for language in LANGUAGE_IDENTIFIERS:
             # Determine whether localizations contains the corresponding language key
             if language not in localizations:
-                if not isInfoPlist:
+                if not is_info_plist:
                     # If not included, use Googletrans to fill in "localizations" after translation.
+                    source_string = (
+                        localizations["en"]["stringUnit"]["value"]
+                        if "en" in localizations
+                        else key
+                    )
                     localizations[language] = {
                         "stringUnit": {
                             "state": "translated",
-                            "value": translate_string(key, language),
+                            "value": translate_string(source_string, language),
                         }
                     }
                 else:
-                   sourceLanguage = json_data["sourceLanguage"]
-                   if sourceLanguage not in localizations:
-                       print("String is empty in source language")
-                       continue
-                   else:
-                    sourceString = localizations[sourceLanguage]["stringUnit"]["value"]
-                    localizations[language] = {
-                        "stringUnit": {
-                            "state": "translated",
-                            "value": translate_string(sourceString, language),
+                    source_language = json_data["sourceLanguage"]
+                    if source_language not in localizations:
+                        print("String is empty in source language")
+                        continue
+                    else:
+                        source_string = (
+                            localizations["en"]["stringUnit"]["value"]
+                            if "en" in localizations
+                            else localizations[source_language]["stringUnit"]["value"]
+                        )
+                        localizations[language] = {
+                            "stringUnit": {
+                                "state": "translated",
+                                "value": translate_string(source_string, language),
+                            }
                         }
-                    }
             else:
-                print(f"Language: {language} has been translated")
+                print(f"{language} has been translated")
 
         strings["localizations"] = localizations
         json_data["strings"][key] = strings
 
         # Save the modified JSON file every time to prevent flashback.
-        with open(json_path, "w") as f:
-            json.dump(json_data, f, indent=4)
+        with open(json_path, "w", encoding='utf-8') as f:
+            json.dump(json_data, ensure_ascii=False, fp=f, indent=4)
 
 def is_infoplist(json_path):
     """
-    判断文件名是否为“InfoPlist”
-
-    Args:
-        json_path: 文件路径
-
-    Returns:
-        True：是“InfoPlist”
-        False：不是“InfoPlist”
+    Determine whether the file name is "InfoPlist.xcstrings" 
+    
+    Args: 
+        json_path: File path 
+    Returns: 
+        True: Yes "InfoPlist.xcstrings" 
+        False: Not "InfoPlist.xcstrings" 
     """
 
     filename = os.path.basename(json_path)
@@ -124,5 +130,5 @@ def is_infoplist(json_path):
 if __name__ == "__main__":
     # Input json_path from terminal
     json_path = input("Enter the string Catalog (.xcstrings) file path:\n")
-    isInfoPlist = is_infoplist(json_path)
+    is_info_plist = is_infoplist(json_path)
     main()
